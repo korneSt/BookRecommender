8280d51ad8c693077c58971f8d94d07b365a81ce
korSt <k4z1kpl@gmail.com>
2016-12-16 14:21:52 +0100

    Using parallax effect

--- src/app/Title.vue
+++ src/app/Title.vue
@@ -1,16 +1,31 @@
 <template>
-  <div class="title">
-    <h1 class="title-h1">'Allo, 'Allo!</h1>
+  <parallax class="parallax" src='/assets/books.jpg' height="300" speed="0.2">
+    <div slot="loading">loading...</div>
     <div>
-      <img class="title-logo" src="http://fountainjs.io/assets/imgs/yeoman.png"/>
-      <img class="title-logo" src="http://fountainjs.io/assets/imgs/fountain.png"/>
+      <div>
+        <h2 class="title-h2">{{ $t('header.info') }}</h2>
+        <img class="title-logo" src="http://fountainjs.io/assets/imgs/yeoman.png"/>
+        <img class="title-logo" src="http://fountainjs.io/assets/imgs/fountain.png"/>
+      </div>
     </div>
-    <h2 class="title-h2">Always a pleasure scaffolding your apps.</h2>
-  </div>
+    </div>
+  </parallax>
 </template>
 
 <script>
 export default {
+  components: {
+  'parallax': require('vue-parallax')
+  },
   name: 'Title'
 };
 </script>
+
+<style>
+ .parallax {
+   margin-top: 50px;
+ } 
+ .title-h2 {
+   color: white;
+ }
+</style>
